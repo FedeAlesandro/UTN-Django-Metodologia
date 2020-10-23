c07d9d54e4e4d6455abefcd5ade74fe6c0709592
Jorge Villordo <villordo235@gmail.com>
2020-10-22 23:23:04 -0300

    Make_filter working and templates

--- rentals/urls.py
+++ rentals/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'rentals'
 urlpatterns = [
     path('', views.estate_filter, name='filter'),
-    path('make_filter/', views.make_filter, name='filter'),
+    path('make_filter/', views.make_filter, name='makefilter'),
     path('home/', views.HomeView.as_view(), name='home'),
     path('details/<int:pk>/', views.DetailView.as_view(), name='details'),
     path('<int:estate_id>/reserve/', views.reserve, name='reserve'),
