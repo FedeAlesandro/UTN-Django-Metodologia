23de76498f769c571968bf5e4863bda20fd2a419
fedealesandro <fede.alesandro@gmail.com>
2020-10-25 16:35:00 -0300

    Views and details template edited

--- rentals/urls.py
+++ rentals/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.estate_filter, name='filter'),
     path('make_filter/', views.make_filter, name='makefilter'),
     path('home/', views.HomeView.as_view(), name='home'),
-    path('details/<int:pk>/', views.DetailView.as_view(), name='details'),
+    path('<int:estate_id>/', views.details, name='details'),
     path('<int:estate_id>/reserve/', views.reserve, name='reserve'),
 ]
--- rentals/views.py
+++ rentals/views.py
@@ -20,11 +20,6 @@ class HomeView(generic.ListView):
         return estates
 
 
-class DetailView(generic.DetailView):
-    model = Estate
-    template_name = 'rentals/details.html'
-
-
 def estate_filter(request):
     today = datetime.date.today()
     today_date = datetime.datetime.strptime(today.__str__(), "%Y-%m-%d").__str__()
@@ -81,6 +76,27 @@ def check_date(since_date, to_date):
     return True
 
 
+def details(request, estate_id):
+    try:
+        print("AAAAAAAAAA")
+        print(date_list)
+        estate = Estate.objects.get(pk=estate_id)
+        subtotal = estate.amount * len(date_list)
+        commission = estate.commission
+        total = subtotal + subtotal*commission
+        context = {
+            'estate': estate,
+            'since_date': date_list[0],
+            'to_date': date_list[-1],
+            'subtotal': subtotal,
+            'commission': commission,
+            'total': total,
+        }
+    except Estate.DoesNotExist:
+        pass
+    return render(request, 'rentals/details.html', context)
+
+
 def reserve(request, estate_id):
     try:
         estate = get_object_or_404(Estate, pk=estate_id)
@@ -94,6 +110,8 @@ def reserve(request, estate_id):
                                       guest_last_name=guest_last_name, guest_email=guest_email)
             reservation.save()
 
+            print("BBBBBBBB")
+            print(date_list)
             for date in date_list:
                 rental_date = RentalDate.objects.filter(date=date, estate=estate_id)
                 rental_date = rental_date[0]
@@ -104,7 +122,6 @@ def reserve(request, estate_id):
             amount = reservation.estate.amount * reservation.estate.rentaldate_set.filter(
                 reservation=reservation).count()
             reservation.amount = amount + amount * estate.commission
-            print(reservation.amount)
             reservation.save()
 
             return HttpResponseRedirect(reverse('rentals:home'))
